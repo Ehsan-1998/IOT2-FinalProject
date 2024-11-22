48617efb1d66772f00368479e17f199351213fc8
Ehsan-1998 <faizeeehsan@gmail.com>
2024-11-22 13:50:40 -0500

    Updated soilMoisture.py

--- Deliverable2/soilMoisture.py
+++ Deliverable2/soilMoisture.py
@@ -1,1 +1,20 @@
 
+import ADC
+import time
+
+def init():
+    ADC.setup()
+
+def run():
+    res = ADC.getADC(0)
+    moisture = 255 - res
+  
+    return moisture
+
+if __name__ == '__main__':
+    init()
+    try:
+        run()
+    except KeyboardInterrupt: 
+        ADC.destroy()
+        print ('The end !')
