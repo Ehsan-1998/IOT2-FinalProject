e27af9cefa0a4f7c62f4c7f3152487838304819b
Ehsan-1998 <faizeeehsan@gmail.com>
2024-12-06 12:12:17 -0500

    Update soilMoisture.py

--- Deliverable2/soilMoisture.py
+++ Deliverable2/soilMoisture.py
@@ -1,20 +1,23 @@
-
-import ADC
+import RPi.GPIO as GPIO  # Import GPIO to manage warnings
+import ADC0832
 import time
 
 def init():
-    ADC.setup()
+    ADC0832.setup()
 
 def run():
-    res = ADC.getADC(0)
+    res = ADC0832.getADC(0)
     moisture = 255 - res
-  
+    print(f"Moisture: {moisture}")
     return moisture
 
 if __name__ == '__main__':
-    init()
+    GPIO.setwarnings(False)  # Disable GPIO warnings
     try:
-        run()
-    except KeyboardInterrupt: 
-        ADC.destroy()
-        print ('The end !')
+        init()
+        while True:
+            run()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        ADC0832.destroy()
+        print('The end!')
